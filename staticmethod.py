42363082851c9b436d39000ab39c04273e0be89c
BDjaekwanee <jaekwanee@gmail.com>
2022-09-27 12:52:42 +0900

    Add : staticmethod 이해하기
    현재 class와, instance 의 self도 사용하지 않는 함수라면 staticmethod 사용
    
    Java 에서는 static 메서드를 class 메서드와 동일시 여겼지만, 파이썬은 아님.

---
+++ staticmethod.py
@@ -0,0 +1,39 @@
+class Robot:
+    """
+    [Robot Class]
+    Author : 송재관
+    Role : ex) 이거 로봇 만든거임.
+    """
+
+    # 클래스 변수
+    population = 0
+
+    # 클래스 함수
+    @classmethod
+    def how_many(cls):
+        print(f"We have {cls.population} robots.")
+
+    # 생성자 함수 : 인스턴스 생성될때 초기화 함수(new랑 같은거 같음 java)
+    def __init__(self, name, code):
+        self.name = name  # 인스턴스 변수
+        self.code = code  # 인스턴스 변수
+        Robot.population += 1
+
+    # 인스턴스 메서드
+    def say_hi(self):
+        print(f"Greetings, my masters call me {self.name}")
+
+    # 인스턴스 메서드
+    def cal_add(self, a, b):
+        return a + b
+
+    def die(self):
+        print("파괴되었습니다.")
+
+    @staticmethod
+    def this_is_root_class():
+        print("yes!!")
+
+
+jaekwan = Robot("jaekwan", 20242141)
+print(jaekwan.this_is_root_class())
